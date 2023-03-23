4132896d099754d356c6d4214ee631ee140a91bc
GuyMickael <mickael.guy.pro@gmail.com>
2023-03-23 15:38:05 +0100

    first steps for posts

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -1,12 +1,36 @@
+import { type User } from "@clerk/nextjs/dist/api";
+import clerkClient from "@clerk/nextjs/server";
 import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
+const filterUserForClient = (user: User) => {
+  return {
+    id: user.id,
+    name: user.username,
+    image: user.profileImageUrl,
+  };
+}
+
 export const postRouter = createTRPCRouter({
   // Get all posts
-  getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.post.findMany();
+  getAll: publicProcedure.query(async ({ ctx }) => {
+    // First we get all posts
+    const posts = await ctx.prisma.post.findMany({
+      take: 100,
+    });
+
+    // Then we get all users to fetch their image and name
+    const userList = ( 
+      await clerkClient.clerkClient.users.getUserList({
+        userId: posts.map((post) => post.authorId),
+        limit: 100,})
+    ).map(filterUserForClient);
+
+    return posts.map((post) => ({
+      post,
+      author: userList.find((user) => user.id === post.authorId),  
+    }));
+
   }),
-
-
 });
