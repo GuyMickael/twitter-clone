6e4dcc9988127195d033ed91c4dfd9e7ff3146ba
MGUY <mickael.guy.pro@gmail.com>
2023-03-26 22:16:38 +0200

    setup creation post

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -3,7 +3,7 @@ import { clerkClient } from "@clerk/nextjs/server";
 import { TRPCError } from "@trpc/server";
 import { z } from "zod";
 
-import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+import { createTRPCRouter, privateProcedure, publicProcedure } from "~/server/api/trpc";
 
 const filterUserForClient = (user: User) => {
   return {
@@ -40,4 +40,17 @@ export const postRouter = createTRPCRouter({
       }
     });
   }),
+
+  create : privateProcedure.input(
+    z.object({
+      content: z.string().emoji().nonempty().max(280)
+    })
+  ).mutation(async ({ ctx, input }) => {
+    const authorId = ctx.session.id;
+
+    const post = await ctx.prisma.post.create({data: {authorId, content: input.content}});
+
+    return post;
+  })
+
 });
